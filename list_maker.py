32856c3a5ee3855cb23c376373a75716047da06f
SuryaThiru <surya.thiru001@gmail.com>
2017-07-09 20:57:49 +0530

    merge attendance data with db

--- list_maker.py
+++ list_maker.py
@@ -5,13 +5,16 @@ import codecs
 # the database will store QA input from a website
 # this website will be used to help management guys contribute to our
 # dataset
-with codecs.open("attendance.txt", "r", "utf-8") as docs_file:
+
+# for file 1
+with codecs.open("attendance1.txt", "r", "utf-8") as docs_file:
     sents = docs_file.readlines()
 
 sents = [x.strip() for x in sents if len(x.strip()) > 0]
 
 questions = []
 answers = []
+
 for index, sent in enumerate(sents):
 
     if index % 2 == 0:
@@ -19,12 +22,40 @@ for index, sent in enumerate(sents):
     elif len(sent) > 0:
         answers.append(sent)
 
-np.save('attendance_Q.npy', questions)
-np.save('attendance_A.npy', answers)
+np.save('attendance_Q1.npy', questions)
+np.save('attendance_A1.npy', answers)
 
 # testing
-questions = np.load('attendance_Q.npy')
-answers = np.load('attendance_A.npy')
-print(questions[:5])
-print(answers[:5])
+questions = np.load('attendance_Q1.npy')
+answers = np.load('attendance_A1.npy')
+print(questions)
+print(answers)
 np.testing.assert_equal(len(questions), len(answers))
+
+
+# for file 2
+with codecs.open("attendance2.txt", "r", "utf-8") as docs_file:
+    sents = docs_file.readlines()
+
+sents = [x.strip() for x in sents if len(x.strip()) > 0]
+
+questions = []
+answers = []
+
+for index, sent in enumerate(sents):
+
+    if index % 2 == 0:
+        questions.append(sent)
+    elif len(sent) > 0:
+        answers.append(sent)
+
+np.save('attendance_Q2.npy', questions)
+np.save('attendance_A2.npy', answers)
+
+# testing
+questions = np.load('attendance_Q2.npy')
+answers = np.load('attendance_A2.npy')
+print(questions)
+print(answers)
+np.testing.assert_equal(len(questions), len(answers))
+
--- makeDB.py
+++ makeDB.py
@@ -1,29 +1,56 @@
 import pymongo
 import numpy as np
+import json
 
 """
 writes list of question and answer from .npys to DB
 """
 
-conn = pymongo.MongoClient('localhost', 27017)
-db = conn.vityBot
-coll = db.attendance
-
-questions = np.load('attendance_Q.npy')
-answers = np.load('attendance_A.npy')
+conn = pymongo.MongoClient('mongodb://chatbot-learn:qwerty1234@ds135592.mlab.com:35592/chatbot-learn')
+db = conn['chatbot-learn']
+coll = db.ques
 
 list_docs = list()
+
+# type 1
+
+f = open('testjsonfolder/db1.json', 'a')
+questions = np.load('attendance_Q1.npy')
+answers = np.load('attendance_A1.npy')
 
 for i in range(len(questions)):
     doc = dict()
 
-    doc['question'] = questions[i]
+    doc['question'] = questions[i].rstrip('?')
     doc['answer'] = answers[i]
+    doc['submitted by'] = 'subham'
+    doc['type'] = 'attendance'
 
     list_docs.append(doc)
+
+    json.dump(doc, f, indent=4)
+    f.write('\n')
     del doc
 
-#clear currrent collection
-coll.drop()
 
-coll.insert_many(list_docs)
+# type 2
+
+f = open('testjsonfolder/db2.json', 'a')
+questions = np.load('attendance_Q2.npy')
+answers = np.load('attendance_A2.npy')
+
+for i in range(len(questions)):
+    doc = dict()
+
+    doc['question'] = questions[i].rstrip('?')
+    doc['answer'] = answers[i]
+    doc['submitted by'] = 'subham'
+    doc['type'] = 'attendance-calculation'
+
+    list_docs.append(doc)
+
+    json.dump(doc, f, indent=4)
+    f.write('\n')
+    del doc
+
+# coll.insert_many(list_docs)
