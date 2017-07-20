8928d1f1c0c4c010bbdc2a2a10fea0e03a6aff96
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-07-20 23:05:52 +0530

    fix #6 minor changes

--- server.py
+++ server.py
@@ -1,30 +1,31 @@
-#Tornado Libraries
+# Tornado Libraries
 from tornado.ioloop import IOLoop
 from tornado.escape import json_encode
 from tornado.web import RequestHandler, Application, asynchronous, removeslash
 from tornado.httpserver import HTTPServer
 from tornado.httpclient import AsyncHTTPClient
-from tornado.gen import engine, Task, coroutine,Return
-#Other Libraries
+from tornado.gen import engine, Task, coroutine, Return
+# Other Libraries
 from motor import MotorClient
 from deep_match import deep_match
 
-#get url from qa repo
+# get url from qa repo
 # url = ..
 db = MotorClient(url)
 # hyperparameters
 threshold = 0.8
+
 
 class MainHandler(RequestHandler):
     @coroutine
     def get(self):
         # define your own question for test
         question = self.get_argument('question')
-        #confidence, question = deep_match(question)
+        # confidence, question = deep_match(question)
         # for now use your custom match we will replace
         # that once we are ready with the server
         if confidence >= threshold:
-            #return db.coll.find_one()
+            # return db.coll.find_one()
             pass
 
 settings = dict(
