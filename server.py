a2455d9327063d721312f65e716e4a6fbce31e1c
shubham0704 <shubham.bhardwaj2015@vit.ac.in>
2017-07-20 22:50:43 +0530

    minor changes

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from tornado.httpclient import AsyncHTTPClient
 from tornado.gen import engine, Task, coroutine,Return
 #Other Libraries
 from motor import MotorClient
+from deep_match import deep_match
 
 #get url from qa repo
 # url = ..
@@ -15,22 +16,20 @@ db = MotorClient(url)
 threshold = 0.8
 
 class MainHandler(RequestHandler):
-	@coroutine
-	def get(self):
-		# define your own question for test
-		question = self.get_argument('question')
-		#confidence, question = deep_match(question)
-		# for now use your custom match we will replace 
-		# that once we are ready with the server
-		if confidence >= threshold:
-			#return db.coll.find_one()
+    @coroutine
+    def get(self):
+        # define your own question for test
+        question = self.get_argument('question')
+        #confidence, question = deep_match(question)
+        # for now use your custom match we will replace
+        # that once we are ready with the server
+        if confidence >= threshold:
+            #return db.coll.find_one()
+            pass
 
 settings = dict(
     db=db,
-    template_path=os.path.join(os.path.dirname(__file__), "templates"),
-    static_path=os.path.join(os.path.dirname(__file__), "static"),
-    debug=True,
-    cookie_secret="35an18y3-u12u-7n10-4gf1-102g23ce04n6"
+    debug=True
 )
 
 application = Application([
