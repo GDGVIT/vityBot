c320c11865f4613cdfdd275c4fbed4e815b58547
Surya <surya.thiru001@gmail.com>
2017-09-05 22:40:07 +0530

    modify best_match findDB

--- modules/NLU/domains/db/find_DB.py
+++ modules/NLU/domains/db/find_DB.py
@@ -1,30 +1,21 @@
 import env
-import sys
 
-from .. import attendance
-from .. import faculty
-from ..timetable import handle_query
 from fuzzywuzzy import fuzz
 import pymongo
 
 host = env.host
 conn = pymongo.MongoClient(host)
 db = conn['vitybot']
-col = db.ques
+col = db['common_examples']
 
 
-def find_all(collection):
-    for d in collection.find():
-        yield d
-
-
-def find_match(query):
+def find_match(query, intent):
     global col
     doc = None
     max_ratio = 0
 
-    for d in find_all(col):
-        ratio = fuzz.token_set_ratio(d['question'], query)
+    for d in col.find({"intent": intent}):
+        ratio = fuzz.token_set_ratio(d['text'], query)
 
         if ratio > max_ratio:
             max_ratio = ratio
@@ -32,29 +23,3 @@ def find_match(query):
 
     del doc['_id']
     return doc['answer']
-
-
-key = ['x_day', 'x_class', 'x_days', 'x_classes']
-
-
-def get_response(query, intent, user):
-    """
-    :return: apt response as string
-    """
-
-    # matched_dbdata = find_match(query)
-
-    if intent == 'faculty-calculation':
-        response = faculty.handle_query.process_query(query)
-        if response:
-            return response
-
-    if intent == 'attendance-calculation':
-        response = attendance.handle_query.process_query(user, query)
-        if response:
-            return response
-
-    if intent == 'timetable-calculation':
-        response = handle_query.process_query(user, query)
-        if response:
-            return response
