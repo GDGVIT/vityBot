d2f3bfdfd8579aed7f3b6797081c6a35b11e9833
Surya <surya.thiru001@gmail.com>
2017-09-06 13:09:24 +0530

    add interface for ws

---
+++ bot.py
@@ -0,0 +1,19 @@
+from modules.NLU import NLU
+from modules.NLU.domains.student_info import get_user
+
+
+class Bot:
+    """
+    the object will act as an interface between the NLU module and
+    the websocket
+    """
+
+    # def __init__(self, username, password):
+    def __init__(self):
+        # self.user = get_user(username, password)
+        self.user = get_user()
+        self.nlu = NLU.NLU()
+
+    def get_reply(self, query):
+        reply = self.nlu.classify(query, self.user)
+        return reply
